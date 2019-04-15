2dfaf52658682d2f53632e6eac8a256b67716cb5
AS Name <sleepless.naeem@gmail.com>
2019-04-15 18:27:38 +0600

    Updates

---
+++ precache-manifest.377c6750018be35207bebadb34e4e5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03ef9a4ca7a9839d06fa",
+    "url": "/static/css/main.1f1fbeec.chunk.css"
+  },
+  {
+    "revision": "03ef9a4ca7a9839d06fa",
+    "url": "/static/js/main.baa556e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "574ac87a0c1f379f4ec2",
+    "url": "/static/css/2.41f979ef.chunk.css"
+  },
+  {
+    "revision": "574ac87a0c1f379f4ec2",
+    "url": "/static/js/2.3aaa3f0e.chunk.js"
+  },
+  {
+    "revision": "dbe2e6f4f56700fc3070951d0685b0a2",
+    "url": "/index.html"
+  }
+];
